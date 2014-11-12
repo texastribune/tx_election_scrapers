73daf57b7fc829f9185839edf881d332d2e1133d
crccheck <c@crccheck.com>
2014-11-12 15:41:33 -0600

    add basic interpreter for realtime statewide results

--- sos/interpret_statewide.py
+++ sos/interpret_statewide.py
@@ -2,14 +2,18 @@
 # -*- coding: UTF-8 -*-
 """
 Usage:
-  cat support/example.html | ./serialize_statewide.py | ./interpret_statewide.py
+  ./interpret_statewide.py [options]
+
+Options:
+  --slugify     Slugify candidate names
+  --indent=<n>  Indent JSON
 
 Interprets and transforms results to make it easier for other scripts to
 consume.
 
 Expects JSON from serialize_statewide.py.
 
-Sample usage:
+Sample:
   cat support/2010_general.html | ./serialize_statewide.py | ./interpret_statewide.py
   cat support/2012_general.html | ./serialize_statewide.py | ./interpret_statewide.py
 """
@@ -17,26 +21,67 @@ from __future__ import unicode_literals
 
 import json
 import sys
-
 from pprint import pprint
 
-
-def coerce_types(obj):
-    """Modifies dict in-place to coerce values."""
-    for k, v in obj.items():
-        if v == 'N/A':
-            obj[k] = None
-        elif k in INT_FIELDS:
-            obj[k] = int(v.replace(',', ''))
+from docopt import docopt
+from utils import int_ish, slugify
 
 
-def coerce_votes(obj):
-    """Modifies dict in-place to coerce values."""
-    return {k: int(v.replace(',', '')) for k, v in obj.items()}
+make_slugs = False
+
+
+def main():
+    options = docopt(__doc__)
+    rows = json.load(sys.stdin)
+
+    # TODO only works for realtime
+    result_keys = ('name', 'party', 'votes_early', 'percent_early', 'votes', 'percent')
+    for race in rows:
+        new_results = []
+
+        # candidate results
+        for result in race['data']:
+            candidate_result = dict(zip(result_keys, result))
+            # TODO get incumbent status
+            candidate_result['slug'] = slugify(candidate_result['name'])
+            candidate_result['votes'] = int_ish(candidate_result['votes'])
+            candidate_result['votes_early'] = int_ish(candidate_result['votes_early'])
+            new_results.append(candidate_result)
+
+        # metadata
+        metadata_raw = race['metadata']
+        if len(metadata_raw) == 5:
+            # pprint(metadata_raw)
+            metadata = {
+                'votes_early': int_ish(metadata_raw[0][2]),
+                'votes': int_ish(metadata_raw[0][4]),
+                'provisional': int_ish(metadata_raw[1][2]),
+                'provisional_early': int_ish(metadata_raw[2][4]),
+                'precincts_reported': int_ish(metadata_raw[3][2]),
+                'precincts_total': int_ish(metadata_raw[3][4].split(' ', 2)[0]),
+                'precincts_reported_percent': metadata_raw[3][6],
+                'statewide_turnout_percent': metadata_raw[4][2],
+                'registered_coters': int_ish(metadata_raw[4][4].split(' ', 2)[0]),
+            }
+            race['metadata'] = metadata
+        elif len(metadata_raw) == 2:
+            metadata = {
+                'votes_early': int_ish(metadata_raw[0][2]),
+                'votes': int_ish(metadata_raw[0][4]),
+                'precincts_reported': int_ish(metadata_raw[1][2]),
+                'precincts_total': int_ish(metadata_raw[1][4].split(' ', 2)[0]),
+                'precincts_reported_percent': metadata_raw[1][6],
+            }
+            race['metadata'] = metadata
+
+        race['data'] = new_results
+
+    # pprint(rows)
+    json.dump(rows, sys.stdout, indent=2)
+
+    global make_slugs  # XXX evil
+    make_slugs = options['--slugify']
 
 
 if __name__ == '__main__':
-    data = json.load(sys.stdin)
-    for race in data:
-        coerce_types(race['metadata'])
-        pprint(race)
+    main()
--- sos/utils.py
+++ sos/utils.py
@@ -13,3 +13,21 @@ def slugify(text):
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
     text = re.sub('[^\w\s-]', '', text).strip().lower()
     return re.sub('[-\s]+', '-', text)
+
+
+def int_ish(numeric_maybe):
+    """
+    HACK quick fix for getting empty things instead of a numeric string
+
+    >>> int_ish('')
+    None
+    >>> int_ish('0')
+    0
+    >>> int_ish('1337')
+    1337
+    >>> int_ish('1,337')
+    1337
+    """
+    if numeric_maybe == '':
+        return None
+    return int(numeric_maybe.replace(',', ''))
