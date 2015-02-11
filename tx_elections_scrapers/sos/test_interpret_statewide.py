624cda00777eb5d48964aa14558bf6d826dacd18
crccheck <c@crccheck.com>
2015-02-11 14:45:33 -0600

    fix how the 'party' row is used for 'delegates' for presidential races

--- tx_elections_scrapers/sos/test_interpret_statewide.py
+++ tx_elections_scrapers/sos/test_interpret_statewide.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(__file__)
 
 
 class RealtimeTest(unittest.TestCase):
-    def test_it_works(self):
+    def test_it_works_2014_general(self):
         html_file = open(os.path.join(BASE_DIR, 'support/rs-2014_general.htm'))
         data = serialize_statewide.serialize(html_file)
         # sanity check
@@ -22,6 +22,31 @@ class RealtimeTest(unittest.TestCase):
         data2 = interpret_statewide.interpret(data)
         # assert modifies in-place
         self.assertEqual(data, data2)
+
+    def test_it_works_2012_rep_primary(self):
+        """
+        Presidential year.
+        """
+        html_file = open(os.path.join(BASE_DIR, 'support/rs-2012_rep_primary.htm'))
+        data = serialize_statewide.serialize(html_file)
+        # sanity check
+        self.assertEqual(data['total_rows'], 160)
+        self.assertEqual(data['election'], '2012 Republican Party Primary Election')
+
+        data = interpret_statewide.interpret(data)  # XXX data = f(data) is evil
+        race_result = data['rows'][0]  # president result
+        self.assertNotIn('header', race_result)
+        self.assertEqual(race_result['data'][0]['name'], 'Michele Bachmann')
+        self.assertEqual(race_result['data'][0]['delegates'], 0)
+        # assert this candidate is missing party info
+        self.assertNotIn('party', race_result['data'][0])
+
+        race_result = data['rows'][1]  # non-president result
+        self.assertNotIn('header', race_result)
+        self.assertEqual(race_result['data'][0]['name'], 'Glenn Addison')
+        self.assertEqual(race_result['data'][0]['party'], 'REP')
+        # assert this candidate is missing delegate info
+        self.assertNotIn('delegates', race_result['data'][0])
 
 
 class HistoricalTest(unittest.TestCase):
